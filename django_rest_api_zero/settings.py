33bf1a2e6765edcbe14cd10854ef24cd248adf14
= <help.studentgrade@gmail.com>
2022-07-16 20:44:05 +0600

    added home app & JsonResponse view

--- django_rest_api_zero/settings.py
+++ django_rest_api_zero/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party library
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
